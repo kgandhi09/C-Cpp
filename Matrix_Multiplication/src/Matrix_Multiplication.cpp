dae93a1b28e0890538306099b9d0bbe4761baacf
kgandhi09 <kushal009gandhi@gmail.com>
2021-02-04 21:07:06 -0500

    Starting Brain-Corp challenge, Matrix Mulitplication using Strassen algorithm

---
+++ Matrix_Multiplication/src/Matrix_Multiplication.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Matrix_Multiplication.cpp
+// Author      : Kushal Gandhi
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "My name is Kushal Gandhi" << endl; // prints !!!Hello World!!!
+	return 0;
+}
